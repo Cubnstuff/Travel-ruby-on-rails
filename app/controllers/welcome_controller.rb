9bb47b37e9e7a1e1727fe7db8628205075e6a703
santiago solana <santiago.sjs@gmail.com>
2016-11-30 17:01:46 +0000

    firstcommit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,12 @@
+class WelcomeController < ApplicationController
+  def index
+    @homestate = 'California'
+    @countries = ['oman', 'argentina', 'New Zeland']
+    @travel_pics = ['peru.jpg', 'forest.jpg', 'city.jpeg', 'rocks.jpeg'] 
+  end
+
+  def about
+    @color = params[:color]
+    @size = params[:size]
+  end
+end
